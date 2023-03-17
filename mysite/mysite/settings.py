bf7c36ae7a00729bc2d57630cfb17aa4f4df6554
TanoR <109026804+Athenator@users.noreply.github.com>
2023-03-17 13:48:27 +0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysiteDb',
-        'USER': 'tanor',
-        'PASSWORD': 'hamidullin',
+        'USER': 'login',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '3306',
     }
